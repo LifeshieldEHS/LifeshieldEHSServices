372ad14639c01f230a08a3eddf8b67c41d595b18
LifeshieldEHS <lifeshield.ehs@gmail.com>
2025-05-28 11:01:09 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+// To Top Button
+
+let topbtn = document.getElementById("topbtn");
+window.onscroll = function () { scrollFunction(); };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbtn.style.display = "block";
+  } else {
+    topbtn.style.display = "none";
+  }
+}
+
+function handleOnClick() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
